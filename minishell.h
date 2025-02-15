eb89f27fb329a698c9e436dc91cad36948b1c16d
a-hakki <abdelazizhatake@gmail.com>
2025-02-15 17:41:31 +0100

    done

--- minishell.h
+++ minishell.h
@@ -6,12 +6,12 @@
 /*   By: ahakki <ahakki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 17:11:01 by ahakki            #+#    #+#             */
-/*   Updated: 2025/02/15 17:14:28 by ahakki           ###   ########.fr       */
+/*   Updated: 2025/02/15 17:41:09 by ahakki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef mini_shell
-# define mini_shell
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
 # include <stdio.h>
 # include <unistd.h>
