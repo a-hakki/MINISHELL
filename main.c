f7c04383359ac75a5d1a9dcd652fb7aa77f96288
a-hakki <abdelazizhatake@gmail.com>
2025-02-17 08:40:06 +0100

    done

--- main.c
+++ main.c
@@ -6,37 +6,109 @@
 /*   By: ahakki <ahakki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 17:10:45 by ahakki            #+#    #+#             */
-/*   Updated: 2025/02/16 20:54:53 by ahakki           ###   ########.fr       */
+/*   Updated: 2025/02/17 08:34:59 by ahakki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int ac, char **av, char **env) {
-	char	*s = readline("minishell>");
-	char	**splited = ft_split(s, ' ');
-	int i = 0;
-	int j = 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+char	*ft_strjoin_with_slash(const char *s1, const char *s2)
+{
+	char	*new_str;
+	int		i, j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	new_str = malloc(strlen(s1) + strlen(s2) + 2);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	new_str[i++] = '/';
+	j = 0;
+	while (s2[j])
+		new_str[i++] = s2[j++];
+	new_str[i] = '\0';
+	return (new_str);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	char	*input;
+	char	**args;
+	char	**paths;
+	char	*cmd_path;
+	int		i;
+	pid_t	pid;
 
 	(void)ac;
 	(void)av;
-	// (void)env;
-	while (splited[i])
-		printf("%s\n", splited[i++]);
-	ft_free("2", splited);
-	i = 0;
-	while (env[i])
+	while (1)
 	{
-		if (!ft_strncmp(env[i], "PATH=", 5))
+		input = readline("minishell> ");
+		if (!input)
+			break;
+		add_history(input);
+		args = ft_split(input, ' ');
+		if (!args)
 		{
-			splited = ft_split(env[i], ':');
-			while (splited[j])
-			{
-				execve(splited[j])
-				printf("%s\n", splited[j++]);
-			}
+			perror("Memory allocation failed");
+			free(input);
+			continue;
 		}
-		i++;
+		i = 0;
+		paths = NULL;
+		while (env[i])
+		{
+			if (!strncmp(env[i], "PATH=", 5))
+			{
+				paths = ft_split(env[i] + 5, ':');
+				break;
+			}
+			i++;
+		}
+		if (!paths)
+		{
+			perror("PATH variable not found");
+			ft_free("2",args);
+			free(input);
+			continue;
+		}
+		pid = fork();
+		if (pid == 0)
+		{
+			i = 0;
+			while (paths[i])
+			{
+				cmd_path = ft_strjoin_with_slash(paths[i], args[0]);
+				execve(cmd_path, args, env);
+				free(cmd_path);
+				i++;
+			}
+			perror("Command not found");
+			exit(127);
+		}
+		else if (pid > 0)
+			wait(NULL);
+		else
+			perror("Fork failed");
+		ft_free("2",args);
+		ft_free("2",paths);
+		free(input);
 	}
 	return (0);
 }
