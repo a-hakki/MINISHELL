bdf2019a5bcf490bb0e141f67306ec44ac5a3869
a-hakki <abdelazizhatake@gmail.com>
2025-05-11 17:38:30 +0100

    export updated

--- built-ins/cd.c
+++ built-ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: ahakki <ahakki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 10:06:14 by ahakki            #+#    #+#             */
-/*   Updated: 2025/05/11 13:04:05 by ahakki           ###   ########.fr       */
+/*   Updated: 2025/05/11 17:37:39 by ahakki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ void	update_env(char **env, char *key, char *nv, t_shell *vars)
 		}
 		i++;
 	}
-	
 }
 
 int	change_dir(char *target, char **envp, t_shell *vars)
@@ -65,18 +64,3 @@ int	cd(int ac, char **av, t_shell *vars)
 	vars->pwd = ft_strdup(get_env("PWD", vars));
 	return (TRUE);
 }
-
-// int main(int ac, char **av, char **env)
-// {
-// 	char	**envp;
-// 	char	*a, *b;
-
-// 	envp = ft_arrdup(env);
-// 	a = getcwd(NULL, 0);
-// 	printf("%s\n", a);
-// 	cd(ac, av, envp);
-// 	b = getcwd(NULL, 0);
-// 	printf("%s\n", b);
-// 	ft_free("11", a, b);
-// 	return (0);
-// }
